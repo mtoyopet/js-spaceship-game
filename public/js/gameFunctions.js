fd3e523d6c34433add4c19e6327fa712cc98f36d
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2020-07-24 19:05:50 +0900

    gameover画面を修正

--- public/js/gameFunctions.js
+++ public/js/gameFunctions.js
@@ -18,7 +18,6 @@ function updateScreen() {
   if (gameStart && gameOver) {
     gameOverText.drawText()
     playAgainText.drawText()
-    restartText.drawText()
   }
 }
 
@@ -42,35 +41,28 @@ function drawPointTextObjects() {
 function keyPressed(event){
   var x = event.keyCode;
 
-  // ゲームが始まっていない状態でスペースキー以外が押された場合
-  if (!gameStart && !gameOver && x !== spaceKey) { return }
-
-  // ゲームが始まっていない状態でスペースキーが押された場合
-  if (!gameStart && !gameOver && x === spaceKey) {
-    gameStart = true
+  if (!gameStart && !gameOver && x !== spaceKey) { 
+    // ゲームが始まっていない状態でスペースキー以外が押された場合
+    return
+  } else if (!gameStart && !gameOver && x === spaceKey) {
+  // ゲームが始まっていない状態でスペースキーが押された場
+  gameStart = true
     return startNewGame()
-  }
-
-  // // ゲームオーバー画面でスペースキーが押された場合
-  // if (gameStart && gameOver && x === spaceKey) {
-  //   gameStart = true
-  //   gameOver = false
-  //   return startNewGame()
-  // }
-
-  // 上方向
-  if (x === upKey) {
-    if (player.position.y <= 0) return player.position.y = 0
-    player.position.y -= 20
-  }
-  // 下方向
-  if (x === downKey) {
-    if (player.position.y >= 380) return player.position.y = 380
-    player.position.y += 20
-  }
-  // スペースキーが押されたら爆弾を発射する
-  if (x === spaceKey) {
-    shotBombs.push(new Bomb("./assets/fire_blue.png", player.position.x + 45, player.position.y + 15))
+  } else {
+    // 上方向
+    if (x === upKey) {
+      if (player.position.y <= 0) return player.position.y = 0
+      player.position.y -= 20
+    }
+    // 下方向
+    if (x === downKey) {
+      if (player.position.y >= 380) return player.position.y = 380
+      player.position.y += 20
+    }
+    // スペースキーが押されたら爆弾を発射する
+    if (x === spaceKey) {
+      shotBombs.push(new Bomb("./assets/fire_blue.png", player.position.x + 45, player.position.y + 15))
+    }
   }
 }
 
